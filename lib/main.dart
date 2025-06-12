790540968653a8f30ecea393e2611d192874e0e7
Ansh Jaiswal <jaiansh2016@gmail.com>
2025-06-12 10:22:02 +0530

    project setup complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:queryly/pages/home_page.dart';
+import 'package:queryly/themes/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false ,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: AppColors.background,
       ),
       home: const HomePage(),
     );
